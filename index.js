72ea7e74afe47d7e06003bd8643b291961b57a66
mattshardman <mattshardman@gmail.com>
2019-03-27 14:59:12 +0000

    add get cohort route

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ const app = mattspress();
 
 app.use(helmet());
 
-app.post('/', (req, res) => {
-    res.send('hi');
+app.get('/api/cohorts', async (req, res) => {
+    try {
+        const cohorts = await db('cohorts');
+        res.status(200);
+        res.json(cohorts);
+    } catch (e) {
+        res.status(400);
+        res.json(e);
+    }
 });
 
 app.listen(3000, () => console.log('Listening'));
