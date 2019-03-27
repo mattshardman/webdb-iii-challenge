8c1ae48c7a50d518a46c79ee45a523f0f3ee4ee9
mattshardman <mattshardman@gmail.com>
2019-03-27 14:33:04 +0000

    add database folder and init knex

---
+++ index.js
@@ -0,0 +1,14 @@
+const mattspress = require('mattspress');
+const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+const app = mattspress();
+
+app.use(helmet());
+
+app.post('/', (req, res) => {
+    res.send('hi');
+});
+
+app.listen(3000, () => console.log('Listening'));
